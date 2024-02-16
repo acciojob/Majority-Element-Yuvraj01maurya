70bb582c57a329450cdd7416505ac548c46d0360
Yuvraj Maurya <yuvraj95166@gmail.com>
2024-02-16 22:03:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your code here
+function majorityElement(nums) {
+    let count = 0;
+    let candidate = null;
+    
+    for (let num of nums) {
+        if (count === 0) {
+            candidate = num;
+        }
+        count += (num === candidate) ? 1 : -1;
+    }
+    
+    return candidate;
+}
+
+// Test cases
+console.log(majorityElement([2, 1, 2])); // Output: 2
